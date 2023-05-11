f0cec3af106f773ffb053b539b170908720895fe
Argo Roots <argo@roots.ee>
2023-05-11 15:09:06 +0300

    Fix focus ring color

--- src/App.vue
+++ src/App.vue
@@ -139,7 +139,7 @@ function findLowestTimeSpan (prices, span) {
       <Listbox v-model="selectedPlan">
         <div class="relative">
           <ListboxButton
-            class="relative w-full cursor-default rounded-lg border bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            class="relative w-full cursor-default rounded-lg border bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-300 sm:text-sm"
           >
             <span class="block truncate">Elektrilevi võrguteenus: {{ selectedPlan.label }}</span>
             <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
