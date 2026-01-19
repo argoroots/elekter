f655fdbd30e4d3f1e4fa0000674ddf0e4437dadf
Argo Roots <argo@roots.ee>
2026-01-19 23:18:17 +0200

    Fix OptionsFlow config_entry property issue

--- custom_components/borsihind/config_flow.py
+++ custom_components/borsihind/config_flow.py
@@ -61,10 +61,6 @@ class ElektrilviConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class ElektrilviOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for Börsihind.ee."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
